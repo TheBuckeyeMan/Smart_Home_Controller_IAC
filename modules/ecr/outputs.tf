69e79abda3b8d966485fb0ed238e04d0905c6c22
Adam Maas <adamjm1220@gmail.com>
2025-01-30 16:54:48 -0500

    Initial Deployment

--- modules/ecr/outputs.tf
+++ modules/ecr/outputs.tf
@@ -1,7 +1,11 @@
 output "aws_ecr_id"{
-    value = aws_ecr_registry.smart_home.id
+    value = aws_ecr_repository.smart_home.id
 }
 
 output "aws_ecr_name"{
-    value = aws_ecr_registry.smart_home.name
+    value = aws_ecr_repository.smart_home.name
+}
+
+output "aws_ecr_arn"{
+    value = aws_ecr_repository.smart_home.arn
 }
