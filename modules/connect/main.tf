167c9269ef0d9ea5a6286a664b0ce4e0078052a8
Adam Maas <adamjm1220@gmail.com>
2025-02-11 09:58:32 -0500

    Updated API Gateway

--- modules/connect/main.tf
+++ modules/connect/main.tf
@@ -1,21 +1,21 @@
 resource "aws_security_group" "smart_home_ec2_security_group" {
     vpc_id = data.aws_vpc.smart_home_vpc.id
 
-    #Specify Inbound HTTP/API Traffic from API Gateway, Lambda, EventBridgen to the Security group
+    #Specify Inbound traffic from API Gateway to the Security group
     ingress {
         from_port = 8080
         to_port = 8080
         protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"] #This will be restricted later in API Gateway ID's
+        cidr_blocks = data.aws_ip_ranges.api_gateway_ip_addresses.cidr_blocks #This will be restricted later in API Gateway ID's
     }
 
     # Allow for SSH for Github Actions to Make Container Updates (Restrict to Github actions ip range)
-    ingress {
-        from_port = 22
-        to_port = 22
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]  # TODO: Restrict this later to API Gateway ID
-    }
+    # ingress {
+    #     from_port = 22
+    #     to_port = 22
+    #     protocol = "tcp"
+    #     cidr_blocks = ["0.0.0.0/0"]  # TODO: Restrict this later to API Gateway ID
+    # }
 
     # Allow all outbound traffic
     egress {
