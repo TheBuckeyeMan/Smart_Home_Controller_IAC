a956c6311f3aeff0aa83fd99d8132c2fa7ae4fc3
Adam Maas <adamjm1220@gmail.com>
2025-02-22 14:35:44 -0500

    Updated shit

--- modules/iot/data.tf
+++ modules/iot/data.tf
@@ -6,3 +6,8 @@ data "aws_iam_role" "smart_home_iot_core_cloudwatch_role" {
 data "aws_iot_endpoint" "iot_endpoint" {
     endpoint_type = "iot:Data-ATS"
 }
+
+# Fetch all active IoT Certificates (needed if existing certs are missing the policy)
+data "aws_iot_certificate" "ec2_existing_certs" {
+    status = "ACTIVE"
+}
