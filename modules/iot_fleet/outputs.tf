d7849893cf9699ffe6cbe4526b82c747f0a2068b
Adam Maas <adamjm1220@gmail.com>
2025-03-02 17:21:04 -0500

    Updated iot_fleet module

--- modules/iot_fleet/outputs.tf
+++ modules/iot_fleet/outputs.tf
@@ -22,33 +22,39 @@ output "iot_fleet_raspberry_pi_termplate_name" {
     value = aws_iot_provisioning_template.smart_home_fleet_template.name
 }
 
-output "iot_fleet_raspberry_pi_disconnect_rule_arn" {
-    value = aws_iot_topic_rule.device_disconnection_alert_rule.arn
-}
+# output "iot_fleet_raspberry_pi_disconnect_rule_arn" {
+#     value = aws_iot_topic_rule.device_disconnection_alert_rule.arn
+# }
 
-output "iot_fleet_raspberry_pi_disconnect_rule_id" {
-    value = aws_iot_topic_rule.device_disconnection_alert_rule.id
-}
+# output "iot_fleet_raspberry_pi_disconnect_rule_id" {
+#     value = aws_iot_topic_rule.device_disconnection_alert_rule.id
+# }
 
-output "iot_fleet_raspberry_pi_disconnect_rule_name" {
-    value = aws_iot_topic_rule.device_disconnection_alert_rule.name
-}
+# output "iot_fleet_raspberry_pi_disconnect_rule_name" {
+#     value = aws_iot_topic_rule.device_disconnection_alert_rule.name
+# }
+
+
 
 # output "iot_fleet_raspberry_pi_disconnect_rule_associated_sns" {
 #     value = aws_iot_topic_rule.device_disconnection_alert_rule.sns[0].target_arn
 # }
 
-output "iot_fleet_raspberry_pi_new_device_rule_arn" {
-    value = aws_iot_topic_rule.device_registration_log_rule.arn
-}
 
-output "iot_fleet_raspberry_pi_new_device_rule_id" {
-    value = aws_iot_topic_rule.device_registration_log_rule.id
-}
 
-output "iot_fleet_raspberry_pi_new_device_rule_name" {
-    value = aws_iot_topic_rule.device_registration_log_rule.name
-}
+# output "iot_fleet_raspberry_pi_new_device_rule_arn" {
+#     value = aws_iot_topic_rule.device_registration_log_rule.arn
+# }
+
+# output "iot_fleet_raspberry_pi_new_device_rule_id" {
+#     value = aws_iot_topic_rule.device_registration_log_rule.id
+# }
+
+# output "iot_fleet_raspberry_pi_new_device_rule_name" {
+#     value = aws_iot_topic_rule.device_registration_log_rule.name
+# }
+
+
 
 # output "iot_fleet_raspberry_pi_new_device_rule_associated_log_group" {
 #     value = aws_iot_topic_rule.device_registration_log_rule.cloudwatch_logs.log_group_name
